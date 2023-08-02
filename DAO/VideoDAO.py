059cb46ba15ee974b657ea30dc2e67af68ba33e7
0duda0 <elpong2@gmail.com>
2023-08-02 20:25:09 +0900

    video request + video list

--- DAO/VideoDAO.py
+++ DAO/VideoDAO.py
@@ -79,3 +79,42 @@ def UpdateVideo_Com(videoPath, videoName, sliceingCount, UserId):
         connection.close()
     except Exception as e:
         raise e
+
+# videoList
+def getListRow(id):
+    try:
+        # dbInfo = DBConnection.dbInfo
+        connection = db.connect(
+            host=dbInfo[0],
+            user=dbInfo[1],
+            port=dbInfo[2],
+            password=dbInfo[3],
+            database=dbInfo[4]
+        )
+        cursor = connection.cursor()
+        sql = "SELECT count(*) FROM videoList WHERE user_id=%s"
+        cursor.execute(sql, (id))
+        row = cursor.fetchone()
+        return row
+
+    except Exception as e:
+        return e
+
+def getList(id):
+    try:
+        # dbInfo = DBConnection.dbInfo
+        connection = db.connect(
+            host=dbInfo[0],
+            user=dbInfo[1],
+            port=dbInfo[2],
+            password=dbInfo[3],
+            database=dbInfo[4]
+        )
+        cursor = connection.cursor()
+        sql = "SELECT * FROM videoList WHERE user_id=%s"
+        cursor.execute(sql, (id))
+        row = cursor.fetchall()
+        return row
+
+    except Exception as e:
+        return e
