0b56baa452a3de61717d7354b462f37714bb1eab
0duda0 <elpong2@gmail.com>
2023-08-09 20:46:25 +0900

    video merge 완료

--- Controller/__init__.py
+++ Controller/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from . import Login
+from . import Login, Video
 from . import Index
 from . import videoList
 
@@ -11,5 +11,10 @@ app.register_blueprint(Index.indexController)
 #로그인/로그아웃 기능 관련 페이지
 app.register_blueprint(Login.loginController)
 
-#영상관련 기능 관련 페이지
+#영상목록 기능 관련 페이지
 app.register_blueprint(videoList.videoListController)
+
+# QR 및 리퀘스트 기능 관련 페이지
+app.register_blueprint(Video.videoController)
+
+
