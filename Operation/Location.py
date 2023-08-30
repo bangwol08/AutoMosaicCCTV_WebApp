e596e8ff52d1c3dead3e6137ddbdb152f5abbca7
bangwol08 <bangwol08@naver.com>
2023-08-30 18:26:01 +0900

    위치확인기능에서 카메라ID가 저장되어 있지 않는 ID라면 영상제작이 진행되지 않도록 수정.

--- Operation/Location.py
+++ Operation/Location.py
@@ -4,6 +4,9 @@ import decimal
 def checkLocation(cameraID,latitude, longitude,errRange):
     row = CameraDAO.SelectCamera(cameraID)
 
+    if row == None:
+        return False
+
     if decimal.Decimal(row[0]) - errRange <= decimal.Decimal(latitude) <= decimal.Decimal(row[0]) + errRange:
         if decimal.Decimal(row[1]) - errRange <= decimal.Decimal(longitude) <= decimal.Decimal(row[1]) + errRange:
             return True
